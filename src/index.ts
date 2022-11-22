7be24821259510f2ae9361a73226c43510b1d655
AvAValera <avavarenik07@gmail.com>
2022-11-22 08:41:35 +0200

    add overload task

--- src/index.ts
+++ src/index.ts
@@ -1,29 +1,37 @@
-interface Man {
+function plus(): string;
+function plus(a: number, b: number): number;
+function plus(a: number, b: number, c: string): number | string;
+function plus(a?: number, b?: number, c?: string | number) {
+    if (!a && !b && !c) {
+        console.log("no argument");
+        return "";
+    } else if (a && b && c && typeof c === "string") {
+        console.log(a + b + " " + c);
+
+        return a + b + " " + c;
+    } else if (a && b && c && typeof c === "number") {
+        console.log(a + b + c);
+        return a + b + c;
+    } else if (a && b) {
+        console.log(a + b);
+        return a + b;
+    }
+}
+
+interface Man  {
     name: string;
     age?: number;
-}
-
-const personFirst: Man = {
-    name: "Well",
-    age: 30,
 };
 
-function checkAge(man: Man): boolean{
-    if(typeof man.age === "number"){
-        return true;
-    }
-    else{
-        return false;
-    }
+function check(man: Man): boolean {
+    if (typeof man.age === "number") return true;
+    else return false;
 }
 
-function showMan(man: Man) {
-    if(checkAge(personFirst)){
+function info(man: Man): void {
+    if(check(man)){
         console.log(man.name, man.age);
     }
-    else{
-        console.log(man.name);
-    }
 }
 
-showMan(personFirst);
+info({name: "Well", age: 30})
