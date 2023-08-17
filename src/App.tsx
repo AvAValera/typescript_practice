5dad4342c7d55854f670cbc60b115584ba12be46
AvAValera <avavarenik07@gmail.com>
2023-08-17 11:04:36 +0200

    start gi users project

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,12 @@
-import "./App.css";
-import Todo from "./components/Todo";
+
 
 function App() {
-    return (
-        <div style={{width: "100%",
-            display: "flex",
-            alignItems: "center",
-            flexDirection: "column",
-            marginTop: "50px"
-        }}>
-            <h1>TODO</h1>
-            <Todo/>
-        </div>
-    );
+
+  return (
+    <>
+
+    </>
+  )
 }
 
-export default App;
+export default App
